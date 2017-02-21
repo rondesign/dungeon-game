0fb12eeb6c41c029c4ecd5a3139a4102dfeb52f3
Ron Lawson <ronlawson@gmail.com>
2017-02-21 22:18:46 +0000

    Add map drawing

--- dungeon_game.py
+++ dungeon_game.py
@@ -37,24 +37,53 @@ def get_moves(player):
     if y == 4:
         moves.remove("DOWN")
     return moves
+    
+def draw_map(player):
+    print(" _"*5)
+    tile = "|{}"
+    
+    for cell in CELLS:
+        x, y = cell
+        if x < 4:
+            line_end = ""
+            if cell == player:
+                output = tile.format("X")
+            else:
+                output = tile.format("_")
+        else:
+            line_end = "\n"
+            if cell == player:
+                output = tile.format("X|")
+            else:
+                output = tile.format("_|")
+        print(output, end=line_end)
+    
+def game_loop():
+    monster, door, player = get_locations()
+    
+    while True:
+        draw_map(player)
+        valid_moves = get_moves(player)
+        print("You're currently in room {}".format(player))
+        print("You can move {}".format(", ".join(valid_moves)))
+        print("Enter QUIT to end game")
+        
+        move = input("> ").upper()
+        
+        if move == 'QUIT':
+            break
+        if move in valid_moves:
+            player = move_player(player, move)
+        else:
+            input("\n ** Walls are hard! Don't run into them! Press return. **\n")
+        clear_screen()
+    
+    
 
-monster, door, player = get_locations()
-    
-while True:
-    valid_moves = get_moves(player)
-    clear_screen()
-    print("Welcome to the dungeon!")
-    print("You're currently in room {}".format(player))
-    print("You can move {}".format(", ".join(valid_moves)))
-    print("Enter QUIT to end game")
-    
-    move = input("> ").upper()
-    
-    if move == 'QUIT':
-        break
-    if move in valid_moves:
-        player = move_player(player, move)
-    else:
-        print("\n ** Walls are hard! Don't run into them! **\n")
-        continue
+clear_screen()
+print("Welcome to the dungeon!")
+input("Press return to start!")
+clear_screen()
+game_loop()
+
     
